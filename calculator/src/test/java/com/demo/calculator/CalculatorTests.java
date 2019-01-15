64cd0d6f9e4fcd376e658861dc0e7f52feb53367
dhruv26patel <dhruv26patel@gmail.com>
2019-01-15 11:28:12 -0600

    Added a unit test

---
+++ calculator/src/test/java/com/demo/calculator/CalculatorTests.java
@@ -0,0 +1,22 @@
+package com.demo.calculator;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTests {
+
+	private CalculatorService  calculator = new CalculatorService();
+	
+	@Test
+	public void testSum() {
+		assertEquals(5, calculator.sum(2,3));
+	}
+
+}
+
