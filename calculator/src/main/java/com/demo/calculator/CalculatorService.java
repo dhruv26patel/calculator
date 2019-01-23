bba0afbf0c32dc48cb4724a666a25d5085ccceb7
dhruv26patel <dhruv26patel@gmail.com>
2019-01-23 13:22:25 -0600

    Added test

--- calculator/src/main/java/com/demo/calculator/CalculatorService.java
+++ calculator/src/main/java/com/demo/calculator/CalculatorService.java
@@ -1,16 +1,15 @@
 package com.demo.calculator;
 
 import org.springframework.stereotype.Service;
-import org.springframework.cache.annotation.Cacheable;
 
 /**
  * Calculator Service.
  */
-
 @Service
 public class CalculatorService {
-    @Cacheable("sum")
-    public int sum(int a, int b) {
-        return a + b;
-    }
+
+	int sum( int a, int b) {
+		return a + b;
+	}
+	
 }
